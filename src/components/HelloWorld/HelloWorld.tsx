bce02281b51653d79f05738c97d1db575c144c53
Jaya Agarwal <jayaagarwal@bnsal.com>
2025-03-24 01:27:26 +0530

    Update HelloWorld.tsx

--- src/components/HelloWorld/HelloWorld.tsx
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import styles from './HelloWorld.module.css';
 
 const HelloWorld: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Hello, World!</h1>
-      <p className={styles.description}>
+    <div >
+      <h1 >Hello, World!</h1>
+      <p >
         This is a basic React application created with Create React App and TypeScript.
       </p>
     </div>
